4071f4bf0429ef37b1e5aa884999d8ac05056d70
josue alfaro <josuealfaroc28@gmail.com>
2026-01-22 18:26:35 -0600

    Semana 1

---
+++ Tienda_JosueAlfaroCamacho/src/main/java/Tienda_JosueAlfaroCamacho/TiendaJosueAlfaroCamachoApplication.java
@@ -0,0 +1,13 @@
+package Tienda_JosueAlfaroCamacho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJosueAlfaroCamachoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJosueAlfaroCamachoApplication.class, args);
+	}
+
+}
---
+++ Tienda_JosueAlfaroCamacho/src/test/java/Tienda_JosueAlfaroCamacho/TiendaJosueAlfaroCamachoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_JosueAlfaroCamacho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJosueAlfaroCamachoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
